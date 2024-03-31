f05b9ac2f3f954f26fa0268d3a523925849a7041
vaishali0211 <saindanevaishali@gmail.com>
2024-03-31 15:30:55 +0530

    static form

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Navigation from "./componets/navigation";
+import Hero from "./componets/Hero";
+
+function App() {
+  return (
+    <div>
+     <Navigation/>
+     <Hero/>
+    </div>
+  );
+}
+
+export default App;
